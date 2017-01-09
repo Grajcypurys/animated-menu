7facff18f3afc29b477ed343864b55bb72a1c283
Grajcypurys <wisniewskimaciej7@gmail.com>
2017-01-09 14:46:13 +0100

    Add files

---
+++ js/main.js
@@ -0,0 +1,46 @@
+
+( function () {
+    "use strict";
+    var btn = document.getElementById("btn-menu")
+    var menu = document.querySelector(".page-nav")
+    var ring = document.querySelector(".hamburger-svg__ring")
+    var ham = document.querySelector(".hamburger")
+    var items = [menu, ring, ham]
+    var itemsClass = ["page-nav__opened", "hamburger-svg__ring-filled", "hamburger-opened"]
+    btn.addEventListener("click", function(){
+        
+        if (ham.className === "hamburger") {
+                btn.setAttribute("title", "Close menu")
+            }
+        else {
+                btn.setAttribute("title", "Open menu")
+            }
+        
+        if (menu.classList) {
+            for (var i = 0; i<items.length; i++){
+                items[i].classList.toggle(itemsClass[i])
+            }
+        }
+        else {
+            ( function () {
+                for (var i = 0, ilen = items.length; i < ilen; i++) {
+                    var reg = new RegExp('(\\s|^)' + itemsClass[i] + '(\\s|$)')
+                    if (items[i].parentNode.nodeName !== "svg") {
+                        if (ham.className === "hamburger") {
+                            items[i].className += " " + itemsClass[i]
+                        } else {
+                            items[i].className = items[i].className.replace(reg, '')
+                        }
+                    } else {
+                        if (ham.className === "hamburger") {
+                            items[i].className.baseVal += " " + itemsClass[i]
+                        } else {
+                            items[i].className.baseVal = items[i].className.baseVal.replace(reg, '')
+                        }
+                    }
+                }
+            } () );
+        }
+        
+    }, false)
+} () );
